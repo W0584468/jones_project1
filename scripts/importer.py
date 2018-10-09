195b9f50325ea86cc1fe5e0ade7d813efecb39d8
DenzelJ <denzel.jones@selu.edu>
2018-10-09 19:38:45 +0000

    first dreaft of scripts and data

---
+++ scripts/importer.py
@@ -0,0 +1,9 @@
+import dendropy
+
+amphib = dendropy.DnaCharacterMatrix.get(
+    path="../data/plethodon.phy",
+    schema="phylip"
+)
+
+amphib.write_to_path("../data/plethodon.fa", schema="fasta")
+
